f0496319aa9e973a1a33829d8e2356ae235dbb2f
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-06-23 22:12:42 +0300

    setup FE and BE with lerna

---
+++ apps/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Greeting} from './Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Greeting />
+      </header>
+    </div>
+  );
+}
+
+export default App;
