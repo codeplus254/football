6ef13489da5c5a3e71d8cf5d5b86f4bb54ab40df
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-06-24 12:29:50 +0300

    Socket.io connected

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const logger = require('@football/logger');
+const socket = require('socket.io');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('/greeting', (req, res) => {
-    logger.info('/greeting was called');
-    res.send({
-        message: `Hello, ${req.query.name || 'World'}!`
-    });
-})
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+const server = app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+
+// socket setup
+const socketIO = socket(server);
+
+socketIO.on('connection', function(socket){
+  console.log('made socket connection', socket.id);
+
+  socket.on('chat', function(data){
+    socketIO.sockets.emit('chat',data)
+  });
+});
