f61559722a640958f446a0bcf377b939b35d9390
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-08-15 15:16:00 +0300

    can now see live updates on screen

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # football
 See live action on the football pitch in form of a graph
+
+Let's go [LIVE](https://football-action.herokuapp.com/)!
--- apps/frontend/src/Greeting.js
+++ apps/frontend/src/Greeting.js
@@ -22,7 +22,7 @@ export class Greeting extends Component {
         }
         socket.emit('chat', name)
          // Listen for events
-        socket.on('chat', (data)=>{this.setState({message: `Hey ${data}` })});
+        socket.on('chat', (data)=>{console.log(data.Incidents);this.setState({message: `LiveUpdates ${data}` })});
     }
     
 
@@ -32,7 +32,7 @@ export class Greeting extends Component {
             <div style={{padding: '10px'}}>
                 {message && <div style={{fontSize: '50px'}}>{message}</div>}
                 <input value={name} onChange={(event) => this.setState({name: event.target.value})} placeholder="Type your name"/>
-                <button onClick={this.onSubmit}>Submit</button>
+                <button onClick={this.onSubmit}>SHOW DATA</button>
                 {error && <pre>{JSON.stringify(error)}</pre>}
             </div>
         );
