77e1100cee7b382b147fc02fcdb16eb27510e37d
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-09-03 16:05:07 +0300

    added header and footer

---
+++ apps/frontend/src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+
+export class Footer extends Component {render() {
+    return (
+      <div className="footer">
+          <a href='#'>About</a>
+          <a href='#'>Add your Bio</a>
+          <a href='#'>Terms and Conditions</a>
+        
+      </div>
+  
+    );
+  }
+}
